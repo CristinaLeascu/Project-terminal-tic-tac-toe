67313bb803ec88ac9685d8ca68fc70ca32455330
Ihoryan <ihorbolzhelarskyi78@gmail.com>
2025-03-01 16:07:11 +0000

    Ihor commit

--- board-printer.js
+++ board-printer.js
@@ -16,7 +16,29 @@
     Test your function by calling it with an example tic-tac-toe board.
 */
 export function printBoard(board) {
-}
+
+    board.forEach(row => {
+        let string = row.join(` | `);
+        
+    
+
+        console.log(string.replaceAll(`_`,` `));
+        console.log(`=========`);
+        })
+    };
+
+
+
+const board = [
+    ['X', '_', '_'],
+    ['_', 'X', '_'],
+    ['O', 'O', 'X']
+];
+// printBoard([
+//     ['X', '_', '_'],
+//     ['_', 'X', '_'],
+//     ['O', 'O', 'X']
+// ]);
 
 /*
     Given a tic-tac-toe board (an array of arrays),
@@ -24,4 +46,24 @@ export function printBoard(board) {
         - return false if there are still moves that can be made
 */
 export function checkIfNoMovesLeft(board) {
+    // board.forEach(row => {
+    //     row.forEach(element => {
+    //         if(element === `_`)return false;
+    //         else return true;
+                
+            
+    //     });
+        
+    // });
+
+    for(let i =0; i<board.length;i++){
+        for(let k=0; i<board[i].length;i++){
+            if(board[i][k]===`_`) return false;
+        
+        }
+    }
+    return true;
+
 }
+
+console.log(checkIfNoMovesLeft(board));
